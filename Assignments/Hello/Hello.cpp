78dee16860aa0120a74d959864e2e9db7dface79
Lilnut05 <144495712+Lilnut05@users.noreply.github.com>
2024-08-22 15:48:21 -0600

    Added Makefile and Added notes in code
    Yes

--- Assignments/Hello/Hello.cpp
+++ Assignments/Hello/Hello.cpp
@@ -1,9 +1,19 @@
+// Single like comment uses a //to indicate a comment
+
+/*
+Author: Adriel Vazquez
+Class CS130
+Date: 8/22/24
+Program Info: A "Hello World" program
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main(void)
 {
+    // The below line outputs the string in quotes to the console
     cout << "Hello World!" << endl;
 
     return 0;
