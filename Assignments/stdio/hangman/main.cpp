dc63713fed103afed538b311b9a4e390d8461b95
Lilnut05 <144495712+Lilnut05@users.noreply.github.com>
2024-09-09 15:53:11 -0600

    Hangman - Homework 1 added

---
+++ Assignments/stdio/hangman/main.cpp
@@ -0,0 +1,25 @@
+/*
+Homework 1 - Hangman
+Author: Adriel Vazquez
+Date: 9/8/24
+This program is an outline of hangman game to be finished in the future.
+Using inputs and outputs to recieve and display characters.
+
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+string name;
+cout <<"Welcome to my hangman program, please enter your name:\n";
+cin >> name;
+cout << "Hello " << name << "! Nice to meet you!";
+cout << "This hangman game is in the works, maybe you\'ll get to see this finished one day. \n Here is a demo of what it will look like!";
+
+string stage0
+stage0 = "    |----------\n    |/    |\n    |\n    |\n    |";
+cout << stage0;
+return 0;
+}
