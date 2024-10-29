f3634e6cccc42d19e61eda4fa3bea66e92a37264
Lilnut05 <144495712+Lilnut05@users.noreply.github.com>
2024-10-28 20:48:35 -0600

    Update main.cpp

--- Assignments/HW6/fallingapart/main.cpp
+++ Assignments/HW6/fallingapart/main.cpp
@@ -5,6 +5,25 @@ HW 6 Kattis Falling Apart
 */
 #include <iostream>
 #include <vector>
-#include <string>
+#include <algorithm>
 
 using namespace std;
+
+
+pair<int, int> splitSum(vector<int> pieces) {
+    sort(pieces.rbegin(), pieces.rend()); // Sort in descending order
+    int player1 = 0, player2 = 0;
+    for (size_t i = 0; i < pieces.size(); i++) {
+        if (i % 2 == 0) player1 += pieces[i];
+        else player2 += pieces[i];
+    }
+    return {player1, player2};
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1 && string(argv[1]) == "test") {
+        // Unit tests
+        vector<pair<vector<int>, pair<int, int>>> testCases = {
+            {{8, 6, 4, 2}, {10, 10}},
+        };
+        
